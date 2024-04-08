c816f541d8bb906ad50299b245966dc60372864d
Abogo <abogonono1@gmail.com>
2024-04-08 02:23:52 +0100

    creation of the  function

--- src/calco.c
+++ src/calco.c
@@ -25,3 +25,16 @@ float subtract(float a, float b)
 {
     return a - b;
 }
+
+/**
+ * sub - subtract two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of @a and @b
+ */
+float subtract(float a, float b)
+{
+    return a - b;
+}
