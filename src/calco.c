cdf5385f34fd6d4a16acf10ee44857f058ccf09f
Abogo <abogonono1@gmail.com>
2024-04-08 02:21:28 +0100

    creation of the add function

---
+++ src/calco.c
@@ -0,0 +1,14 @@
+#include "../inc/calco.h"
+
+/**
+ * add - add two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of @a and @b
+ */
+float add(float a, float b)
+{
+    return a + b;
+}
