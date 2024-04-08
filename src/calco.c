5b135417e6a9adad3a88ee5c8a1c6e280b6af847
Abogo <abogonono1@gmail.com>
2024-04-08 02:22:48 +0100

    creation of the subtract function

--- src/calco.c
+++ src/calco.c
@@ -12,3 +12,16 @@ float add(float a, float b)
 {
     return a + b;
 }
+
+/**
+ * sub - subtract two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of @a and @b
+ */
+float subtract(float a, float b)
+{
+    return a - b;
+}
